a9320677caf827ba3754be08325b70738b570d4c
Michał Boruch <miboruch@gmail.com>
2020-02-29 13:58:04 +0100

    Added fonts, created global style

--- src/components/Layout.tsx
+++ src/components/Layout.tsx
@@ -1,13 +1,19 @@
 import React from 'react';
 import { ThemeProvider } from 'styled-components';
 import { theme } from '../styles/theme';
+import GlobalStyle from '../styles/GlobalStyle';
 
 interface Props {
   appTheme?: string;
 }
 
 const Layout: React.FC<Props> = ({ appTheme, children }) => {
-  return <ThemeProvider theme={theme}>{children}</ThemeProvider>;
+  return (
+    <>
+      <GlobalStyle />
+      <ThemeProvider theme={theme}>{children}</ThemeProvider>
+    </>
+  );
 };
 
 export default Layout;
